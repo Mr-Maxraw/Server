e57d5413b33b9cf3b9a2e7eddf3e2dd2f7a794d8
Петухов Максим <maxraw111@gmail.com>
2020-08-04 07:21:46 +0500

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Client
+    {
+        public Socket _socket { get; set; }
+        public ReceivePacket Receive { get; set; }
+        public int Id { get; set; }
+
+        public Client(Socket socket, int id)
+        {
+            Receive = new ReceivePacket(socket, id);
+            Receive.StartReceiving();
+            _socket = socket;
+            Id = id;
+        }
+    }
+
+    static class ClientController
+    {
+        public static List<Client> Clients = new List<Client>();
+
+        public static void AddClient(Socket socket)
+        {
+            Clients.Add(new Client(socket, Clients.Count));
+        }
+
+        public static void RemoveClient(int id)
+        {
+            Clients.RemoveAt(Clients.FindIndex(x => x.Id == id));
+        }
+    }
+
+    public class ReceivePacket
+    {
+        private byte[] _buffer;
+        private Socket _receiveSocket;
+        private int _clientId;
+
+        public ReceivePacket(Socket receiveSocket, int id)
+        {
+            _receiveSocket = receiveSocket;
+            _clientId = id;
+        }
+
+        public void StartReceiving()
+        {
+            try
+            {
+                _buffer = new byte[4];
+                _receiveSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveCallback, null);
+            }
+            catch { }
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                // if bytes are less than 1 takes place when a client disconnect from the server.
+                // So we run the Disconnect function on the current client
+                if (_receiveSocket.EndReceive(AR) > 1)
+                {
+                    // Convert the first 4 bytes (int 32) that we received and convert it to an Int32 (this is the size for the coming data).
+                    _buffer = new byte[BitConverter.ToInt32(_buffer, 0)];
+                    // Next receive this data into the buffer with size that we did receive before
+                    _receiveSocket.Receive(_buffer, _buffer.Length, SocketFlags.None);
+                    // When we received everything its onto you to convert it into the data that you've send.
+                    // For example string, int etc... in this example I only use the implementation for sending and receiving a string.
+
+                    // Convert the bytes to string and output it in a message box
+                    string data = Encoding.Default.GetString(_buffer);
+                    Console.WriteLine(data);
+                    // Now we have to start all over again with waiting for a data to come from the socket.
+                    StartReceiving();
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+            catch
+            {
+                // if exeption is throw check if socket is connected because than you can startreive again else Dissconect
+                if (!_receiveSocket.Connected)
+                {
+                    Disconnect();
+                }
+                else
+                {
+                    StartReceiving();
+                }
+            }
+        }
+
+        private void Disconnect()
+        {
+            // Close connection
+            _receiveSocket.Disconnect(true);
+            // Next line only apply for the server side receive
+            ClientController.RemoveClient(_clientId);
+            // Next line only apply on the Client Side receive
+            //Here you want to run the method TryToConnect()
+        }
+    }
+
+    class Listener
+    {
+        public Socket ListenerSocket;
+        public int Port = 1234;
+
+        public Listener()
+        {
+            ListenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void StartListening()
+        {
+            try
+            {
+                Console.WriteLine($"Listening started port:{Port} protocol type: {ProtocolType.Tcp}");
+                ListenerSocket.Bind(new IPEndPoint(IPAddress.Any, Port));
+                ListenerSocket.Listen(10);
+                ListenerSocket.BeginAccept(AcceptCallback, ListenerSocket);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("error" + e);
+            }
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Console.WriteLine($"Accept CallBack port:{Port} protocol type: {ProtocolType.Tcp}");
+                Socket acceptedSocket = ListenerSocket.EndAccept(ar);
+                ClientController.AddClient(acceptedSocket);
+                ListenerSocket.BeginAccept(AcceptCallback, ListenerSocket);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("accept error" + e);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Listener listener = new Listener();
+            listener.StartListening();
+        }
+    }
+}
